20f0cb8354fcfc3d3beca26a47c6ce7d23768b1f
Xinze-Li-Moqian <70414198+Xinze-Li-Moqian@users.noreply.github.com>
2026-08-13 20:55:59 -0400

    Match the license header line the style linter expects
    The header linter wants the second copyright line to name LICENSE
    exactly; lean/LICENSE tripped it in all 43 source files, and lean/
    has its own LICENSE anyway.

--- lean/HessenbergDigraphs/Command.lean
+++ lean/HessenbergDigraphs/Command.lean
@@ -1,6 +1,6 @@
 /-
 Copyright (c) 2026 Xinze Li, Simone Severini. All rights reserved.
-Released under Apache 2.0 license as described in the file lean/LICENSE.
+Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Xinze Li, Simone Severini
 -/
 import HessenbergDigraphs.Render
--- lean/HessenbergDigraphs/Matrix/Hessenberg.lean
+++ lean/HessenbergDigraphs/Matrix/Hessenberg.lean
@@ -1,6 +1,6 @@
 /-
 Copyright (c) 2026 Simone Severini. All rights reserved.
-Released under Apache 2.0 license as described in the file lean/LICENSE.
+Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Simone Severini
 -/
 import Util.Linter.MathTag
